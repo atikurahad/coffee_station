5868e62ffb7419e2bb2fc99fb385953c81a3932c
Atikur Rahaman <atikurahad@gmail.com>
2022-05-18 00:28:13 +0600

    review section added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import { Route, Routes } from 'react-router-dom';
-import About from './About';
-import './App.css';
-import Contact from './Contact';
-import Foot from './Footer';
-import Home from './Home';
-import Navigation from './Navigation';
-import NotFound from './NotFound';
+import { Route, Routes } from "react-router-dom";
+import About from "./About";
+import "./App.css";
+import Contact from "./Contact";
+import Foot from "./Footer";
+import Home from "./Home";
+import Navigation from "./Navigation";
+import NotFound from "./NotFound";
+import Review from "./Review";
 
 function App() {
   return (
     <div>
       <Navigation />
       <Routes>
-        {/* <Route path="/" element={<Layout />}></Route> */}
         <Route index element={<Home />} />
-        <Route path='/home' element={<Home />} />
+        <Route path="/home" element={<Home />} />
 
         <Route path="/about" element={<About />} />
+        <Route path="/review" element={<Review />} />
+
         <Route path="/contact" element={<Contact />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
-      {/* <Shop /> */}
-    <Foot />
+      <Foot />
     </div>
   );
 }

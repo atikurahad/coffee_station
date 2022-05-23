e3c3005cd8ded4ac4e98c765cdecf6ce1b1402d6
Atikur Rahaman <atikurahad@gmail.com>
2022-05-24 00:07:00 +0600

    review section styled

--- src/Review/index.js
+++ src/Review/index.js
@@ -4,17 +4,16 @@ const Review = () => {
   return (
     <div className="flex justify-evenly gap-4 mt-5 ]">
       <div className="flex justify-center">
-        <div className="block rounded-lg shadow-lg bg-white max-w-sm text-center">
-          <div className="py-3 px-6 border-b border-gray-300 bg-slate-500">
+        <div className=" bg-black block rounded-lg shadow-lg max-w-sm text-center">
+          <div className=" text-[gold] py-3 px-6 border-b border-gray-300 bg-black">
             Review
           </div>
           <div className="p-6">
-            <h5 className="text-blue-900 font-bold text-xl mb-2">
-              Sholayman Sojib
-            </h5>
-            <p className="text-gray-700 text-2xl mb-4">
-              With supporting text below as a natural lead-in to additional
-              content.
+            <h5 className="text-black bg-[yellow] font-bold text-xl mb-2">
+              Virat Kohli
+                          </h5>
+            <p className="text-[yellow] font-mono mb-4">
+             Coffee can  heal on my mind.This Coffee Station is one of my favourite coffee shop.Stay connected with Coffee Station.
             </p>
 
             <ul className="flex justify-center ">
@@ -106,7 +105,7 @@ const Review = () => {
             </ul>
           </div>
           <div className="py-3 px-6 border-t border-gray-300 text-gray-600">
-            2 days ago
+            2 hour ago
           </div>
         </div>
       </div>
@@ -115,10 +114,10 @@ const Review = () => {
         <div className="block rounded-lg shadow-lg bg-[black] max-w-sm text-center">
           <div className="py-3 px-6 text-[gold] font-extrabold border-b border-gray-300 bg-[black]">Review</div>
           <div className="p-6">
-            <h5 className="text-[gold] bg-[yellow] text-xl font-medium mb-2">
-            Kareena Kaporr
+            <h5 className="text-[black] bg-[yellow] text-xl font-medium mb-2">
+            Kareena Kapoor
             </h5>
-            <p className="text-gray-700 text-base mb-4">
+            <p className="text-[yellow] font-mono mb-4">
               With supporting thjis coffee station ,This is the best coffee station in this town. The are excellent with Coffee and its flavour.
             </p>
             <ul className="flex justify-center ">
@@ -222,7 +221,7 @@ const Review = () => {
             <h5 className="bg-[yellow] text-gray-900 text-xl font-medium mb-2">
               Salman Khan
             </h5>
-            <p className=" text-[gold] text-base mb-4">
+            <p className=" text-[gold] font-mono mb-4">
               One of the best  coffe i had ever taste.Service and behavior are excellent. I am fully satisfied of this coffe house.
             </p>
             <ul className="flex justify-center ">

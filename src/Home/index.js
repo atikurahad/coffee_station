8660d965798418e7c7521a3a76e8a44f69d8eb29
Atikur Rahaman <atikurahad@gmail.com>
2022-05-25 23:30:36 +0600

    about are more detailed

--- src/Home/index.js
+++ src/Home/index.js
@@ -5,19 +5,19 @@ import './Home.css'
 const Home = () => {
   return (
     <div>
-      <h1 className="coffee">Welcome To Coffee Station</h1>
+      <h1 className="text-red-400 m-5  text-center font-extrabold text-4xl ">Welcome To Coffee Station</h1>
 
-      <p className='coffee-benefit'>
-        Another study in 126 older adults found that drinking coffee was
-        associated with improved physical performance and faster gait speed,
-        even after the researchers adjusted for factors like age, belly fat, and
-        physical activity levels.
+      <p className="m-5 p-4 bg-[#EAE7DE] text-stone-900 text-2xl font-black border-2 ">
+        Coffee processing refers to the way that a seed is removed from a coffee
+        cherry. Like any other pitted fruit, coffee cherries have a seed, the
+        pulp, the mucilage around the seed, and a protective skin. There are
+        various methods to remove the seed from that cherry, and these methods
+        affect the seed's flavor as it gets roasted and turned into a coffee
+        bean—this flavor sticks around till the final brew.
       </p>
-     <Shop />
+      <Shop />
 
-    <div>
-
-    </div>
+      <div></div>
 
       {/* <a href="google.com">
         <img
